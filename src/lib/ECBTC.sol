eee92dd74b3e4f3234982e7651e465450df71f3f
nzmpi <nizami.abdulov@gmail.com>
2024-05-10 20:21:19 +0400

    feat: update ECBTC

--- src/lib/ECBTC.sol
+++ src/lib/ECBTC.sol
@@ -2,8 +2,15 @@
 pragma solidity ^0.8.24;
 
 import "./EllipticCurve.sol";
+import {Point} from "./Structs.sol";
 
+/**
+ * @title Secp256k1 Elliptic Curve
+ * @dev A wrapper for elliptic curve operations using secp256k1 parameters.
+ */
 library ECBTC {
+    using EllipticCurve for uint256;
+
     uint256 constant GX = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
     uint256 constant GY = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8;
     uint256 constant AA = 0;
@@ -11,23 +18,33 @@ library ECBTC {
     uint256 constant PP = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
     uint256 constant N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
 
-    struct Point {
-        uint256 x;
-        uint256 y;
+    /**
+     * @dev Modulo N
+     * @param _x - The number
+     * @return _x mod N
+     */
+    function mod(uint256 _x) internal pure returns (uint256) {
+        return _x % N;
     }
 
     /**
-     * @dev Multiplies scalar and an ec point
-     * @param _k - The scalar
-     * @param _p - The point
-     * @return p - The result
+     * @dev Adds two scalars mod N
+     * @param _x - The first scalar
+     * @param _y - The second scalar
+     * @return res - The result
      */
-    function mul(uint256 _k, Point calldata _p) internal pure returns (Point memory p) {
-        (p.x, p.y) = EllipticCurve.ecMul(_k, _p.x, _p.y, AA, PP);
+    function add(uint256 _x, uint256 _y) internal pure returns (uint256 res) {
+        res = addmod(_x, _y, N);
     }
 
+    /**
+     * @dev Multiplies two scalars mod N
+     * @param _x - The first scalar
+     * @param _y - The second scalar
+     * @return res - The result
+     */
     function mul(uint256 _x, uint256 _y) internal pure returns (uint256 res) {
-        res = (_x * _y) % N;
+        res = mulmod(_x, _y, N);
     }
 
     /**
@@ -36,7 +53,26 @@ library ECBTC {
      * @return p - The result
      */
     function mulG(uint256 _k) internal pure returns (Point memory p) {
-        (p.x, p.y) = EllipticCurve.ecMul(_k, GX, GY, AA, PP);
+        (p.x, p.y) = _k.ecMul(GX, GY, AA, PP);
+    }
+
+    /**
+     * @dev Inverts a scalar, such as x * xInv = 1 mod N
+     * @param _x - The scalar
+     * @return xInv - The result
+     */
+    function inv(uint256 _x) internal pure returns (uint256 xInv) {
+        xInv = _x.invMod(N);
+    }
+
+    /**
+     * @dev Multiplies scalar and an ec point
+     * @param _k - The scalar
+     * @param _p - The point
+     * @return p - The result
+     */
+    function mul(uint256 _k, Point memory _p) internal pure returns (Point memory p) {
+        (p.x, p.y) = _k.ecMul(_p.x, _p.y, AA, PP);
     }
 
     /**
@@ -46,10 +82,6 @@ library ECBTC {
      * @return p - The result
      */
     function add(Point memory _p1, Point memory _p2) internal pure returns (Point memory p) {
-        (p.x, p.y) = EllipticCurve.ecAdd(_p1.x, _p1.y, _p2.x, _p2.y, AA, PP);
-    }
-
-    function inv(uint256 _x) internal pure returns (uint256 xInv) {
-        xInv = EllipticCurve.invMod(_x, N);
+        (p.x, p.y) = _p1.x.ecAdd(_p1.y, _p2.x, _p2.y, AA, PP);
     }
 }
