507db54a524efa4758bc1d72cb8932ba860e2c5e
nzmpi <nizami.abdulov@gmail.com>
2024-05-17 21:21:49 +0400

    test: add the serialize signature test

--- test/SerialLib.t.sol
+++ test/SerialLib.t.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.24;
 
 import "./utils/BaseTest.sol";
 import {SerialLib} from "../src/lib/SerialLib.sol";
+import {SigLib} from "../src/lib/SigLib.sol";
+import {Signature} from "../src/lib/Structs.sol";
 
 contract TestSerialLib is BaseTest {
     using SerialLib for Point;
+    using SerialLib for Signature;
     using SerialLib for bytes;
+    using SigLib for uint256;
 
     function test_serializePublicKey() public view {
         bytes memory result = pubKey.serializePublicKey(false);
@@ -41,5 +45,19 @@ contract TestSerialLib is BaseTest {
         PKSerlial = hex"fff3423acb";
         vm.expectRevert(SerialLib.WrongPrefix.selector);
         PKSerlial.parsePublicKey();
+    }
+
+    function test_serializeSignature() public view {
+        Signature memory sig = messageHash.sign(privateKey);
+        bytes memory result = sig.serializeSignature();
+        bytes memory expected =
+            hex"3046022100bed017bc6f4c80dbffd97ae753b58b597c1b71334de814cd59f482303b29425f022100966d4754727615e27d969afa11a72f95bd92280981dee958f50fce3555578245";
+        assertEq(result, expected, "Signature should be serialized correctly");
+
+        sig = anotherMessageHash.sign(anotherPrivateKey);
+        result = sig.serializeSignature();
+        expected =
+            hex"304402203bb237e75196b68bec2c415c2efde31364557a50681df03bbc1b6bd3dfcb4d4602203a3ec12e15d72a071b042a4e59cf52ec4f5b2fc664d3ff5f82be08c32e2d4553";
+        assertEq(result, expected, "Another signature should be serialized correctly");
     }
 }
