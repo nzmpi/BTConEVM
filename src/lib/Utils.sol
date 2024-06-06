edd2031b51ab50f9a3740d81641b3c6695d9ec24
nzmpi <nizami.abdulov@gmail.com>
2024-06-07 03:01:06 +0400

    feat: add the convertion from bytes to uint256 and vice versa

--- src/lib/Utils.sol
+++ src/lib/Utils.sol
@@ -7,6 +7,8 @@ pragma solidity ^0.8.24;
  * @author https://github.com/nzmpi
  */
 library Utils {
+    error WrongLength();
+
     /**
      * Hash160
      * @param _data - The data to hash
@@ -36,6 +38,50 @@ library Utils {
         res = new bytes(len);
         for (uint256 i; i < len; ++i) {
             res[i] = _data[len - 1 - i];
+        }
+    }
+
+    /**
+     * Converts bytes to uint256
+     * @dev Reverts if length is greater than 32
+     * @param _data - The data to be converted
+     * @return res - The converted data
+     */
+    function bytesToUint256(bytes memory _data) internal pure returns (uint256 res) {
+        uint256 len = _data.length;
+        if (len == 0 || len > 32) revert WrongLength();
+
+        // padding zeros
+        bytes memory temp = new bytes(32);
+        for (uint256 i; i < len; ++i) {
+            temp[i + 32 - len] = _data[i];
+        }
+
+        return uint256(bytes32(temp));
+    }
+
+    /**
+     * Converts uint256 to bytes
+     * @param _data - The data to be converted
+     * @return res - The converted data
+     */
+    function uint256ToBytes(uint256 _data) internal pure returns (bytes memory res) {
+        res = bytes.concat(bytes32(_data));
+
+        // counting leading zeroes
+        uint256 zeroes;
+        for (uint256 i; i < 32; ++i) {
+            if (res[i] == 0x00) {
+                ++zeroes;
+            } else {
+                break;
+            }
+        }
+        uint256 len = 32 - zeroes;
+        assembly {
+            mstore(res, len)
+            let start := add(res, 32)
+            mcopy(start, add(start, zeroes), len)
         }
     }
 }
