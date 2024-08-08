749cc3c3996ee541df985621644114a7cefa0bf6
nzmpi <nizami.abdulov@gmail.com>
2024-08-09 03:54:07 +0400

    feat: add Script types

---
+++ src/lib/Types.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/**
+ * @notice Represents the supported type of a script
+ */
+enum ScriptType {
+    P2PKH, // Pay-to-public-key-hash
+    P2SH // Pay-to-script-hash
+}
