fb90588da455b5aebc7c4205f41b455f6478091c
nzmpi <nizami.abdulov@gmail.com>
2024-08-09 22:43:16 +0400

    fix: remove length for transaction params

--- test/SerialLib.t.sol
+++ test/SerialLib.t.sol
@@ -136,7 +136,7 @@ contract TestSerialLib is BaseTest {
         assertEq(resTx.inputs[0].vout, hex"00000001", "Wrong vout 0");
         assertEq(
             resTx.inputs[0].scriptSig,
-            hex"d90047304402203285484f30722560cfd2c8f346c58be14791ac2859b9ef5aebd31373b9d9ed3802202a40bf31a4a297ba3130920707a2d5315019f33cd175270c198e81727460c96401473044022001888d61eb5d3d5528098531a7dbdb5233748712f069f022a2135e6175bb66a9022064860cb4fb71c3879dce3835765b7f76dbf35621f55729b8c69623e55d13811f01475221022b6dc4281a12a37a00ab920fd9ab1d4b509e288122cf406cd3d0bfb2f5471706210392e4ecdd0af74a2d05b9448343b22fac173c1618c62caf2039efb09c7e67f2d452ae",
+            hex"0047304402203285484f30722560cfd2c8f346c58be14791ac2859b9ef5aebd31373b9d9ed3802202a40bf31a4a297ba3130920707a2d5315019f33cd175270c198e81727460c96401473044022001888d61eb5d3d5528098531a7dbdb5233748712f069f022a2135e6175bb66a9022064860cb4fb71c3879dce3835765b7f76dbf35621f55729b8c69623e55d13811f01475221022b6dc4281a12a37a00ab920fd9ab1d4b509e288122cf406cd3d0bfb2f5471706210392e4ecdd0af74a2d05b9448343b22fac173c1618c62caf2039efb09c7e67f2d452ae",
             "Wrong scriptSig 0"
         );
         assertEq(resTx.inputs[0].sequence, hex"ffffffff", "Wrong sequence 0");
@@ -144,13 +144,13 @@ contract TestSerialLib is BaseTest {
         assertEq(resTx.outputs[0].amount, hex"00000000034985b0", "Wrong amount 0 at 0");
         assertEq(
             resTx.outputs[0].scriptPubKey,
-            hex"17a914b4eef9a952db30866728f788bb469bae8d5fc52687",
+            hex"a914b4eef9a952db30866728f788bb469bae8d5fc52687",
             "Wrong scriptPubKey 0 at 0"
         );
         assertEq(resTx.outputs[1].amount, hex"000000003787dfd5", "Wrong amount 0 at 1");
         assertEq(
             resTx.outputs[1].scriptPubKey,
-            hex"17a91483e02ab859c6ab1e4ee45b20492eaca173f0b6fb87",
+            hex"a91483e02ab859c6ab1e4ee45b20492eaca173f0b6fb87",
             "Wrong scriptPubKey 0 at 1"
         );
         assertEq(resTx.locktime, hex"00000000", "Wrong locktime 0");
@@ -162,13 +162,13 @@ contract TestSerialLib is BaseTest {
             resTx.inputs[0].txId, hex"0000000000000000000000000000000000000000000000000000000000000000", "Wrong txId 1"
         );
         assertEq(resTx.inputs[0].vout, hex"ffffffff", "Wrong vout 1");
-        assertEq(resTx.inputs[0].scriptSig, hex"0f04bedf061a02d405062f503253482f", "Wrong scriptSig 1");
+        assertEq(resTx.inputs[0].scriptSig, hex"04bedf061a02d405062f503253482f", "Wrong scriptSig 1");
         assertEq(resTx.inputs[0].sequence, hex"ffffffff", "Wrong sequence 1");
         assertEq(resTx.outputs.length, 1, "Wrong number of outputs 1");
         assertEq(resTx.outputs[0].amount, hex"000000012a05f200", "Wrong amount 1");
         assertEq(
             resTx.outputs[0].scriptPubKey,
-            hex"43410450d5ef5bb3d8a893a4b1fcf2e9d8f898d782fbf22a887f3fbafd22b06d225a5754be9b2cd0d00ee2c3715bfced2e403a6e0d1bf2716ebfd6e6f9ddac1924b6e7ac",
+            hex"410450d5ef5bb3d8a893a4b1fcf2e9d8f898d782fbf22a887f3fbafd22b06d225a5754be9b2cd0d00ee2c3715bfced2e403a6e0d1bf2716ebfd6e6f9ddac1924b6e7ac",
             "Wrong scriptPubKey 1"
         );
         assertEq(resTx.locktime, hex"00000000", "Wrong locktime 1");
@@ -184,7 +184,7 @@ contract TestSerialLib is BaseTest {
         assertEq(resTx.inputs[0].vout, hex"00000000", "Wrong vout 2 at 0");
         assertEq(
             resTx.inputs[0].scriptSig,
-            hex"6c493046022100ed23c8cbb485acd3c073137803584b2a2c91f350d17b54bd1a10a4b814dfad44022100a567ba44f3a3b0769ae96b9af42f4a33827eaa92f5c40deb5886e0009a70f39d012102e2d7f7486ced79f3a4fcc26e4848bd2de484397ba8df365d468acbeecd3562b8",
+            hex"493046022100ed23c8cbb485acd3c073137803584b2a2c91f350d17b54bd1a10a4b814dfad44022100a567ba44f3a3b0769ae96b9af42f4a33827eaa92f5c40deb5886e0009a70f39d012102e2d7f7486ced79f3a4fcc26e4848bd2de484397ba8df365d468acbeecd3562b8",
             "Wrong scriptSig 2 at 0"
         );
         assertEq(resTx.inputs[0].sequence, hex"ffffffff", "Wrong sequence 2 at 0");
@@ -196,7 +196,7 @@ contract TestSerialLib is BaseTest {
         assertEq(resTx.inputs[1].vout, hex"00000000", "Wrong vout 2 at 1");
         assertEq(
             resTx.inputs[1].scriptSig,
-            hex"6b483045022100e7ad48a20853c7cc1f419589407173b5e82c37e5715f7bc08ed6682381b0adcc0220200ec21da4a684f870bc8065f8a92e669421ca4c747278cc7a0aa5ad44b6555401210342dd1c67748c439ba4567a325c842caa3a16ba5200e730c47af2e5aa581903ed",
+            hex"483045022100e7ad48a20853c7cc1f419589407173b5e82c37e5715f7bc08ed6682381b0adcc0220200ec21da4a684f870bc8065f8a92e669421ca4c747278cc7a0aa5ad44b6555401210342dd1c67748c439ba4567a325c842caa3a16ba5200e730c47af2e5aa581903ed",
             "Wrong scriptSig 2 at 1"
         );
         assertEq(resTx.inputs[1].sequence, hex"ffffffff", "Wrong sequence 2 at 1");
@@ -208,7 +208,7 @@ contract TestSerialLib is BaseTest {
         assertEq(resTx.inputs[2].vout, hex"00000000", "Wrong vout 2 at 2");
         assertEq(
             resTx.inputs[2].scriptSig,
-            hex"6b483045022100b473b18f9942a9097e5e8677fd4c0442458c193770a65de258928c9f1d9478e902201f41799ef679577fdf0b2783962d1f64da1f828d994991afdff5465bfd8970e9012102f14306fcfc9a8138cc86469c5d2b40f28d885a19faf45b2be6578ea10e910252",
+            hex"483045022100b473b18f9942a9097e5e8677fd4c0442458c193770a65de258928c9f1d9478e902201f41799ef679577fdf0b2783962d1f64da1f828d994991afdff5465bfd8970e9012102f14306fcfc9a8138cc86469c5d2b40f28d885a19faf45b2be6578ea10e910252",
             "Wrong scriptSig 2 at 2"
         );
         assertEq(resTx.inputs[2].sequence, hex"ffffffff", "Wrong sequence 2 at 2");
@@ -216,13 +216,13 @@ contract TestSerialLib is BaseTest {
         assertEq(resTx.outputs[0].amount, hex"0000000017d78400", "Wrong amount 2 at 0");
         assertEq(
             resTx.outputs[0].scriptPubKey,
-            hex"1976a914f5bc94a48ad7f290c58b4fb970514cbf99daf30b88ac",
+            hex"76a914f5bc94a48ad7f290c58b4fb970514cbf99daf30b88ac",
             "Wrong scriptPubKey 2 at 0"
         );
         assertEq(resTx.outputs[1].amount, hex"00000000000f4247", "Wrong amount 2 at 1");
         assertEq(
             resTx.outputs[1].scriptPubKey,
-            hex"1976a914f094dc8e530cdc17221fc936399fcf0da5c3cb4a88ac",
+            hex"76a914f094dc8e530cdc17221fc936399fcf0da5c3cb4a88ac",
             "Wrong scriptPubKey 2 at 1"
         );
         assertEq(resTx.locktime, hex"00000000", "Wrong locktime 2");
