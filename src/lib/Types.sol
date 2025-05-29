8496d865220216da987cdc2cf85174d326c8e45f
nzmpi <nizami.abdulov@gmail.com>
2025-05-29 19:47:42 +0400

    feat: add P2PK

--- src/lib/Types.sol
+++ src/lib/Types.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.24;
  * @notice Represents the supported type of a script
  */
 enum ScriptType {
+    P2PK, // Pay-to-public-key
     P2PKH, // Pay-to-public-key-hash
     P2SH, // Pay-to-script-hash
     P2WPKH, // Pay-to-witness-public-key-hash
